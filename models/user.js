3cf87f5385350319f989e837481d56a32302a93b
SDavila18023 <sdavilaa@unbosque.edu.co>
2025-02-08 17:04:51 -0500

    nuevos datos bd

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 
+
 /**
  * Esquema de un usuario en la base de datos.
  * @typedef {Object} UserSchema
@@ -48,12 +49,28 @@ const userSchema = new mongoose.Schema(
     photo: { type: String }, // URL de la foto del usuario
     genposition: { type: String }, // Posición general del usuario
     natposition: { type: [String] }, // Posiciones naturales del usuario
-    yearsexp: { type: Number }, // Años de experiencia
+    yearsexp: [
+      {
+        startYear: { type: Number, required: true },
+        endYear: { type: Number }, // Si está vacío, el jugador sigue activo en el club
+        club: { type: String, required: true },
+        description: { type: String }
+      }
+    ],
     description: { type: String }, // Descripción o biografía
     foot: { type: String }, // Pie dominante
     typeid: { type: String }, // Tipo de identificación
     identification: { type: Number, unique: true }, // Número de identificación
     videos: { type: [String] }, // Videos asociados
+    injuryHistory: {type: Number}, // Historial de lesiones
+    trainingHoursPerWeek: {type: Number}, // Horas de entrenamiento por semana
+    achievements: [
+      {
+        year: { type: Number, required: true }, // Año del logro
+        title: { type: String, required: true }, // Nombre o título del logro
+        type: { type: String, enum: ["trophy", "medal", "star"], required: true } // Tipo de logro
+      }
+    ],    
     resetPasswordToken: { type: String }, // Token de restablecimiento
     resetPasswordExpires: { type: Date }, // Expiración del token
     score: { type: Number }, // Puntuación general
@@ -66,10 +83,17 @@ const userSchema = new mongoose.Schema(
   }
 );
 
+
+
+
 /**
  * Modelo de Mongoose para la colección de usuarios.
  * @typedef {mongoose.Model} User
  */
 const User = mongoose.model("User", userSchema);
 
+
 export default User;
+
+
+
